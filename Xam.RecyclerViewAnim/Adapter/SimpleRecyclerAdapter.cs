4071bfad8bd00105c3867a47fee31e57609c3015
sancho <developersancho@gmail.com>
2018-01-07 19:48:20 +0300

    ---
    ---

---
+++ Xam.RecyclerViewAnim/Adapter/SimpleRecyclerAdapter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using static Android.Support.V7.Widget.RecyclerView;
+
+namespace Xam.RecyclerViewAnim.Adapter
+{
+
+    public class SimpleViewHolder : ViewHolder
+    {
+
+        public TextView textView { get; set; }
+
+        public SimpleViewHolder(View itemView) : base(itemView)
+        {
+            textView = itemView.FindViewById<TextView>(Resource.Id.textView);
+        }
+    }
+
+    public class SimpleRecyclerAdapter : RecyclerView.Adapter
+    {
+        List<int> dataSource;
+
+        public SimpleRecyclerAdapter()
+        {
+            dataSource = new List<int>();
+            for (int i = 0; i < 50; i++)
+            {
+                dataSource.Add(i);
+            }
+        }
+
+        public override int ItemCount => dataSource.Count;
+
+        public override void OnBindViewHolder(ViewHolder holder, int position)
+        {
+            SimpleViewHolder vh = holder as SimpleViewHolder;
+            vh.textView.Text = dataSource[position].ToString();
+        }
+
+        public override ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View view = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.ListItem, parent, false);
+            return new SimpleViewHolder(view);
+        }
+    }
+}
---
+++ Xam.RecyclerViewAnim/MainActivity.cs
@@ -0,0 +1,68 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.Widget;
+using Xam.RecyclerViewAnim.Adapter;
+using System;
+using Android.Views.Animations;
+
+namespace Xam.RecyclerViewAnim
+{
+    [Activity(Label = "Xam.RecyclerViewAnim", MainLauncher = true, Icon = "@mipmap/icon"
+              //Theme = "@style/Theme.AppCompact.Light.NoActionBar"
+             )]
+    public class MainActivity : Activity
+    {
+        RecyclerView recyclerView;
+        Button btnFallDown, btnSlideBottom, btnFromLeft;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            btnFallDown = FindViewById<Button>(Resource.Id.btnFallDown);
+            btnFallDown.Click += delegate {
+                RunAnimation(recyclerView, 0);
+            };
+
+            btnSlideBottom = FindViewById<Button>(Resource.Id.btnSlideBottom);
+            btnSlideBottom.Click += delegate {
+                RunAnimation(recyclerView, 1);
+            };
+
+            btnFromLeft = FindViewById<Button>(Resource.Id.btnFromLeft);
+            btnFromLeft.Click += delegate {
+                RunAnimation(recyclerView, 2);
+            };
+
+            recyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
+           
+           
+        }
+
+        private void RunAnimation(RecyclerView recycView, int type)
+        {
+            var context = recycView.Context;
+            LayoutAnimationController controller = null;
+
+            if (type == 0)
+                controller = AnimationUtils.LoadLayoutAnimation(context, Resource.Animation.layout_fall_down);
+            else if (type == 1)
+                controller = AnimationUtils.LoadLayoutAnimation(context, Resource.Animation.layout_slide_from_bottom);
+            else if (type == 2)
+                controller = AnimationUtils.LoadLayoutAnimation(context, Resource.Animation.layout_slide_from_left);
+            
+            recyclerView.SetLayoutManager(new GridLayoutManager(this,4));
+            SimpleRecyclerAdapter adapter = new SimpleRecyclerAdapter();
+            recyclerView.SetAdapter(adapter);
+
+            recyclerView.LayoutAnimation = controller;
+            recyclerView.GetAdapter().NotifyDataSetChanged();
+            recyclerView.ScheduleLayoutAnimation();
+        }
+    }
+}
+
